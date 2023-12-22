f704296bf4d24e0bdaafbefcc710e59c4455951a
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-12-22 20:23:32 +0530

    Update dependencies and remove unused code

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,11 +7,11 @@
 
 
 
-	<!-- bootstrap -->
+	{{-- <!-- bootstrap -->
 	<link rel="stylesheet" href=" {{ asset('assests/bootstrap/css/bootstrap.min.css') }} ">
   
 	<!-- bootstrap theme-->
-	<link rel="stylesheet" href="{{ asset('assests/bootstrap/css/bootstrap-theme.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assests/bootstrap/css/bootstrap-theme.min.css') }}"> --}}
 
 
 
@@ -29,14 +29,8 @@
   <link rel="stylesheet" href="{{ asset('assests/plugins/datatables/jquery.dataTables.min.css') }}">
 
 
-
   <!-- file input -->
   <link rel="stylesheet" href="{{ asset('assests/plugins/fileinput/css/fileinput.min.css') }}">
-
-
-
-  <!-- jquery -->
-	<script src="{{ asset('assests/jquery/jquery.min.js') }}"></script>
 
 
   <!-- jquery ui -->  
@@ -45,9 +39,44 @@
 
 {{-- bootstrap select  --}}
 
-<link rel="stylesheet" href=" {{ asset('assests/bootstrap-select/css/bootstrap-select.min.css') }}">
+{{-- <link rel="stylesheet" href=" {{ asset('assests/bootstrap-select/css/bootstrap-select.min.css') }}"> --}}
 
 <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css"/>
+
+
+<!-- jquery -->
+<script src="{{ asset('assests/jquery/jquery.min.js') }}"></script>
+
+<!-- file input -->
+<script src="{{ asset('assests/plugins/fileinput/js/plugins/canvas-to-blob.min.js') }}" type="text/javascript"></script>	
+<script src="{{ asset('assests/plugins/fileinput/js/plugins/sortable.min.js') }}" type="text/javascript"></script>	
+<script src="{{ asset('assests/plugins/fileinput/js/plugins/purify.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assests/plugins/fileinput/js/fileinput.min.js') }}"></script>	
+
+
+<!-- DataTables -->
+<script src="{{ asset('assests/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+
+<!-- Bootstrap -->
+
+<script src="{{ asset('assests/bootstrap/js/bootstrap.min.js')}}"></script>
+
+<!-- Bootstrap select  -->
+<script src="{{ asset('assests/bootstrap-select/js/bootstrap-select.min.js') }}"></script>
+
+
+
+<!-- JQuery UI -->
+<script src="{{ asset('assests/jquery-ui/jquery-ui.min.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
 
 
